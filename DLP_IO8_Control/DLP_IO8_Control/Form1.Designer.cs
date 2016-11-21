1eba4766babe2222210027ac22ad0fcfd994a0b2
ingmacmech <marcus.schluep@kns.ch>
2016-11-21 23:14:41 +0100

    First working test

--- DLP_IO8_Control/DLP_IO8_Control/Form1.Designer.cs
+++ DLP_IO8_Control/DLP_IO8_Control/Form1.Designer.cs
@@ -29,11 +29,75 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.labelValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(25, 227);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(187, 227);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(197, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 2;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // labelValue
+            // 
+            this.labelValue.AutoSize = true;
+            this.labelValue.Location = new System.Drawing.Point(22, 90);
+            this.labelValue.Name = "labelValue";
+            this.labelValue.Size = new System.Drawing.Size(35, 13);
+            this.labelValue.TabIndex = 3;
+            this.labelValue.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.labelValue);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Label labelValue;
     }
 }
 
--- DLP_IO8_Control/DLP_IO8_Control/Form1.cs
+++ DLP_IO8_Control/DLP_IO8_Control/Form1.cs
@@ -5,16 +5,93 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace DLP_IO8_Control
 {
     public partial class Form1 : Form
     {
+        bool keepGoing = true;
+        string value;
+
+
+
         public Form1()
         {
             InitializeComponent();
+            ToggleState();
+            initSerial();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            ToggleState();
+
+            serialPort.Close();
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            ToggleState();
+            serialPort.Open();
+            //serialPort.WriteLine("\\");
+            //serialPort.WriteLine("'");
+            StartRead();
+        }
+
+        private void ToggleState()
+        {
+            // Flip the value of the loop control flag  
+            keepGoing = !keepGoing;
+
+            // Enable/disable buttons  
+            buttonStop.Enabled = keepGoing;
+            buttonStart.Enabled = !keepGoing;
+        }
+        private void StartRead()
+        {
+
+            // Set up a new thread to do the counting  
+            ThreadStart ts = new ThreadStart(Count);
+            Thread t = new Thread(ts);
+
+            // Start the thread  
+            t.Start();
+
+            // While this thread is running, the form will continue to respond  
+        }
+        private void Count()
+        {
+            // Loop until stopped or the number reaches its maximum value  
+            while (keepGoing)
+            {
+                serialPort.WriteLine("X");
+                try
+                {
+                    value = serialPort.ReadLine();
+                    //label5.Invoke((MethodInvoker)(() => label5.Text = "Requested" + repeats + "Times"));
+                    labelValue.Invoke((MethodInvoker)(() => labelValue.Text = value));
+                }
+                catch (TimeoutException) { }
+                // Pause for 100 milliseconds  
+                Thread.Sleep(1);
+            }
+
+        }
+
+        private void initSerial()
+        {
+            serialPort.PortName = "COM7";
+            serialPort.BaudRate = 115200;
+            serialPort.ReadTimeout = 50;
+            serialPort.WriteTimeout = 50;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
